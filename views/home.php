3a5e790fcf5acb190ee9d7f9fbe183aaa5ae540a
lucaslagant <lucaslagant@gmail.com>
2021-12-23 04:57:16 +0100

    new homepage

--- views/home.php
+++ views/home.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-    <div class="row">        
+    <!-- <div class="row">        
         <div class="col-6 d-flex justofy-content-center">
             <h1>A propos</h1>
             <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error reprehenderit eligendi quod officia doloremque. Ullam, reiciendis ducimus. Magnam eius exercitationem voluptatem voluptatum, blanditiis assumenda nesciunt! Laboriosam repellat temporibus minima sint?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore harum maiores tempora voluptatem, ducimus veniam adipisci aperiam temporibus quas aut perspiciatis, ratione beatae molestias vero vitae repudiandae, dolorem voluptates! Sed?</p>
@@ -7,7 +7,16 @@
         <div class="col-3">
             <img  class="imgNoel"src="/assets/img/ongle5rogner1.jpg" alt="ongleNoel">
         </div>
-    </div>    
+    </div>     -->
+    <div class="row">
+        <div class="about col-6">
+            <h1>A Propos</h1>
+            <p class="text">SSnails est une onglerie ouverte aux début de l'année 2022 . Nous avons à coeur de rendre vos ongles tendances avec un style unique et personalisé toutes en vous proposant les soins qui l'accompagne , en effet pour nous la santé de vos ongles compte aussi.</p>
+        </div>
+        <div class="col-6">
+            <img src="/assets/img/" alt="">
+        </div>
+    </div>
    
     
 </div>
